93c3d0f02fa45b6a3be1763acef633a7ad6ee60c
NikoGraci <nikograci1122@gmail.com>
2022-06-12 12:39:57 +0200

    Added a file

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  return a + b;
+}
+console.log(add(3, 3));
+
+function sub(a, b) {
+  return a - b;
+}
+console.log(sub(3, 3));
+
+function div(a, b) {
+  return a / b;
+}
+console.log(div(3, 3));
+
+function multi(a, b) {
+  return a * b;
+}
+console.log(multi(3, 3));
